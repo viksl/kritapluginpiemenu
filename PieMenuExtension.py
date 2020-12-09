d3c3cb3a6def24426ddbb5d0dedaf141905e00d0
viksl <33526847+viksl@users.noreply.github.com>
2020-12-09 20:54:24 +0100

    Update PieMenuExtension.py

--- PieMenuExtension.py
+++ PieMenuExtension.py
@@ -1,5 +1,14 @@
 from krita import *
 
+class Dialog(QDialog):
+  def __init__(self, text, parent=None):
+      super(Dialog, self).__init__(parent)
+      self.setLayout(QVBoxLayout())
+      self.label = QLabel(str(text))
+      self.layout().addWidget(self.label)
+      self.resize(200, 50)
+      self.exec_()
+
 class PieMenuExtension(Extension):
   def __init__(self,parent):
     super(PieMenuExtension, self).__init__(parent)
