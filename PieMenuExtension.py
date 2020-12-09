a4fa3724c445e64b7d62de59d9d995267c4d65b3
viksl <33526847+viksl@users.noreply.github.com>
2020-12-09 21:12:14 +0100

    test

--- PieMenuExtension.py
+++ PieMenuExtension.py
@@ -4,9 +4,10 @@ from .MenuArea import MenuArea
 class PieMenuExtension(Extension):
   def __init__(self,parent):
     super(PieMenuExtension, self).__init__(parent)
-    self.menuArea = MenuArea(QCursor.pos(), window.qwindow())
+
   def setup(self):
     pass
 
   def createActions(self, window):
     self.PMAction = window.createAction("pieMenu", "Pie Menu")
+    self.menuArea = MenuArea(QCursor.pos(), window.qwindow())
