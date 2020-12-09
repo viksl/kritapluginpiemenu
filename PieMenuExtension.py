ca00d472440a643f4d86242bac94c5e662110531
viksl <33526847+viksl@users.noreply.github.com>
2020-12-09 21:34:20 +0100

    Update PieMenuExtension.py

--- PieMenuExtension.py
+++ PieMenuExtension.py
@@ -5,12 +5,24 @@ from .MenuArea import MenuArea
 class PieMenuExtension(Extension):
   def __init__(self,parent):
     super(PieMenuExtension, self).__init__(parent)
+    self.settings = None
+    self.menuArea = None
 
   def setup(self):
     pass
 
+  def openPieMenu( self ):
+    self.menuArea = MenuArea(QCursor.pos(), Krita.instance().activeWindow().qwindow())
+
+  def openSettings( self ):
+    self.settings = Settings(Krita.instance().activeWindow().qwindow())
+
   def createActions(self, window):
     self.pieMenuAction = window.createAction("pieMenu", "Pie Menu")
     self.pieMenuSettingsAction = window.createAction("pieMenuSettings", "Pie Menu Settings")
-    self.settings = Settings(window.qwindow())
-    self.menuArea = MenuArea(QCursor.pos(), window.qwindow())
+
+    self.pieMenuAction.triggered.connect(self.openPieMenu)
+    self.pieMenuAction.setAutoRepeat(False)
+
+    self.pieMenuSettingsAction.triggered.connect(self.openSettings)
+    self.pieMenuSettingsAction.setAutoRepeat(False)
