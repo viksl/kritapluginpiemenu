042efd44efbb16c043a1aff7e74b9e1dbba1ce1f
viksl <lempikq@gmail.com>
2020-12-23 17:38:18 +0100

    Fix for TabletRelease event (some systems don't use MouseRelease here) now both work with a safety.

--- plugin/pykrita/kritapluginpiemenu/MenuArea.py
+++ plugin/pykrita/kritapluginpiemenu/MenuArea.py
@@ -50,6 +50,7 @@ class EventController(QMdiArea):
         self.eventObj = eventObj
         self.controllerOwner = controllerOwner
         self.mouseButtonPress = False
+        self.buttonReleased = False
 
     def eventFilter(self, source, event):
         """
@@ -93,7 +94,10 @@ class EventController(QMdiArea):
             
             return True
 
-        elif event.type() == QEvent.MouseButtonRelease:
+        elif (( event.type() == QEvent.MouseButtonRelease or event.type() == QEvent.TabletRelease )
+            and self.buttonReleased == False
+        ):
+            self.buttonReleased = True
             self.mouseButtonPress = False
             self.deleteEventFilter(source, event)
             return True
@@ -109,6 +113,7 @@ class EventController(QMdiArea):
             or event.type() == QEvent.KeyPress
             or event.type() == QEvent.MouseButtonPress
             or event.type() == QEvent.MouseButtonRelease
+            or event.type() == QEvent.TabletRelease
         ):
             
             return True
@@ -120,7 +125,9 @@ class EventController(QMdiArea):
             self.eventObj.ResetGUI()
             self.eventObj.hide()
 
-            if event.type() != QEvent.MouseButtonRelease:
+            if (event.type() != QEvent.MouseButtonRelease
+                and event.type() != QEvent.TabletRelease
+            ):
                 self.controllerOwner.keyReleased = True
                 self.removeEventFilter(self)
                 self.controllerOwner.eventController.deleteLater()
@@ -217,7 +224,7 @@ class PieMenu(QWidget):
         QApplication.processEvents()
 
     def eventHandler(self, event, keyReleased=False):
-        if event.type() == QEvent.MouseButtonRelease:
+        if event.type() == QEvent.MouseButtonRelease or event.type() == QEvent.TabletRelease:
             if self.resetCallback != None:
                 self.resetCallback()
                 return
